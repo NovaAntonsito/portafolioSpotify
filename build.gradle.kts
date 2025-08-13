b2c93091a91035a67d094b98ad32c07ff06e6734
NovaAntonsito <marcos.anton@alumnos.frm.utn.edu.ar>
2025-08-13 16:34:31 -0300

    Cambios en los .kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,8 +10,11 @@ version = "0.0.1-SNAPSHOT"
 java {
 	toolchain {
 		languageVersion = JavaLanguageVersion.of(21)
+		vendor = JvmVendorSpec.ADOPTIUM
+		implementation = JvmImplementation.VENDOR_SPECIFIC
 	}
 }
+
 
 configurations {
 	compileOnly {
